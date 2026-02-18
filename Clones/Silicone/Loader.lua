e3b376786bb5755de0f0d8c360dac67cf5b9634d
skibidihook <skibidihook@gmail.com>
2026-02-18 03:48:41 +0000

    e
    e

---
+++ Clones/Silicone/Loader.lua
@@ -0,0 +1,16 @@
+local Games = {
+    [286090429] = "",
+    [7711635737] = "",
+    [891852901] = "",
+    [136801880565837] = "",
+    [94539932414164] = "",
+    [142823291] = "",
+    [6161235818] = "",
+    [5104202731] = "",
+    [89570211388424] = ""
+}
+
+local LoaderUrl = Games[game.PlaceId]
+if LoaderUrl then
+    loadstring(game:HttpGet(LoaderUrl))()
+end
