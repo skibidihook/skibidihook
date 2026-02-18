fce37095fc1d04385e506fc70f9f761d00cafcba
skibidihook <skibidihook@gmail.com>
2026-02-18 04:29:34 +0000

    Update Loader.lua

--- Clones/Silicone/Loader.lua
+++ Clones/Silicone/Loader.lua
@@ -1,18 +1,17 @@
 print("CRACKED BY SIGMA X CORD X ACHE")
+local Repo = "https://raw.githubusercontent.com/ghgghigher-prog/Silicon-leak/main/Clones/Silicone/"
 local Games = {
-    [286090429] = "",
-    [7711635737] = "",
-    [891852901] = "",
-    [136801880565837] = "",
-    [94539932414164] = "",
-    [142823291] = "",
-    [6161235818] = "",
-    [5104202731] = "",
-    [89570211388424] = ""
+    [286090429] = Repo .. "Arsenal.lua",
+    [7711635737] = Repo .. "EmergencyHamburg.lua",
+    [891852901] = Repo .. "Greenville.lua",
+    [136801880565837] = Repo .. "Flick.lua",
+    [94539932414164] = Repo .. "MojaveValley.lua",
+    [142823291] = Repo .. "MurderMystery2.lua",
+    [6161235818] = Repo .. "Twisted.lua",
+    [5104202731] = Repo .. "SouthwestFlorida.lua",
+    [89570211388424] = Repo .. "CentralKansas.lua"
 }
-
 local LoaderUrl = Games[game.PlaceId]
 if LoaderUrl then
     loadstring(game:HttpGet(LoaderUrl))()
 end
-print("CRACKED BY SIGMA X CORD X ACHE")
