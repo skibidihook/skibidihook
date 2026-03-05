f3d939fd233895ee89ce700cdd695bc268cef4ec
skibidihook <skibidihook@gmail.com>
2026-03-05 01:36:37 +0000

    e
    e

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,6 +1,7 @@
 local Games = {
     [7336302630] = "https://raw.githubusercontent.com/skibidihook/skibidihook/main/Games/Project%20Delta/obfuscated.lua",
-    [113217312262185] = "https://raw.githubusercontent.com/skibidihook/skibidihook/main/Games/Scp%Retrobreach/obfuscated.lua"
+    [113217312262185] = "https://raw.githubusercontent.com/skibidihook/skibidihook/main/Games/Scp%Retrobreach/obfuscated.lua",
+    [109397169461300] = "https://raw.githubusercontent.com/skibidihook/skibidihook/main/Games/Sniper%Duels/obfuscated.lua"
 }
 
 local LoaderUrl = Games[game.PlaceId]
