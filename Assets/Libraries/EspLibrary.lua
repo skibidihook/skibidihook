29daf794bf86a2809a7b7c02152b6651ca117e1a
skibidihook <skibidihook@gmail.com>
2026-05-02 19:06:55 +0100

    wrwer
    rwerwe

--- Assets/Libraries/EspLibrary.lua
+++ Assets/Libraries/EspLibrary.lua
@@ -1,7 +1,7 @@
 local CloneRef = cloneref or function(...) return ... end
 local Workspace = CloneRef(game:GetService("Workspace"))
 local CurrentCamera = CloneRef(Workspace.CurrentCamera)
-local WorldToViewportPoint = CloneRef(CurrentCamera.WorldToViewportPoint)
+local WorldToViewportPoint = CurrentCamera.WorldToViewportPoint
 
 local DrawingNew = Drawing.new
 local Vector2New = Vector2.new
